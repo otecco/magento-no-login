25df0bf47f97eea8abd1a71daa9452050870c700
Jan Tomka <jan@shopix.com.au>
2013-12-05 13:18:38 +1100

    Fix PHP tags and license comments.

--- code/Block/Checkout/Onepage.php
+++ code/Block/Checkout/Onepage.php
@@ -16,8 +16,6 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-?>
-<?php
 
 class Shopix_Nologin_Block_Checkout_Onepage extends Mage_Checkout_Block_Onepage
 {
--- code/Model/Checkout/Type/Onepage.php
+++ code/Model/Checkout/Type/Onepage.php
@@ -16,8 +16,6 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-?>
-<?php
 
 class Shopix_Nologin_Model_Checkout_Type_Onepage extends Mage_Checkout_Model_Type_Onepage
 {
